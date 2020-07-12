653fea5b8d3efed33375b9df1e9a67c80d173dd2
dy <dy@qq.com>
2020-07-12 19:12:11 +0800

    first commit

---
+++ src/main/java/com/dy/ci/entities/Apple.java
@@ -0,0 +1,21 @@
+package com.dy.ci.entities;
+
+public class Apple {
+	
+	private String name = "Good Apple";
+	private String color = "red";
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+}
